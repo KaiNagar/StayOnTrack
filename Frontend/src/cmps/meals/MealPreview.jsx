594215fd2d37ddec8943e5d980b375ac7b1c9b93
Kai Nagar <kaynagar@gmail.com>
2023-02-05 15:44:25 +0200

    added dynamic calc for total cals and display

--- Frontend/src/cmps/meals/MealPreview.jsx
+++ Frontend/src/cmps/meals/MealPreview.jsx
@@ -1,17 +1,20 @@
+import { IngredientsList } from '../ingredients/IngredientsList'
 import { MealCal } from './MealCal'
-import { MealIngredients } from './MealIngredients'
+// import { MealIngredients } from './MealIngredients'
 
 export const MealPreview = ({ meal }) => {
+  console.log(meal);
   // cal service calculation
   return (
     <section className='meal-preview flex column '>
+      <h1 className='meal-title'>{meal.name}</h1>
       <div className='meal-img'>
-        <img src={meal.imgUrl} alt='' />
+        <img src={meal.imgUrl} alt='Meal Img' />
       </div>
       <div className='meal-stats flex column space-between'>
-        <MealIngredients ing={meal.ingredients} />
+        <IngredientsList ings={meal.ingredients} />
         {/* <div className="divider"></div> */}
-        <MealCal cal={meal.totalCal} />
+        {/* <MealCal cal={meal.totalCal} /> */}
       </div>
     </section>
   )
