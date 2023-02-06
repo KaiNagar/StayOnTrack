7dcb71cdfe8aaae3934160e1f5102fc5a1f74056
Kai Nagar <kaynagar@gmail.com>
2023-02-06 21:19:34 +0200

    started meal add + edit form

--- Frontend/src/pages/MealEdit.jsx
+++ Frontend/src/pages/MealEdit.jsx
@@ -1,49 +1,88 @@
+import { Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { useForm } from 'react-hook-form'
 import { mealService } from '../services/meal.service'
+import { saveMeal } from '../store/actions/mealActions'
 
 export const MealEdit = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-    reset,
-  } = useForm()
-  const { ingredients } = useSelector((state) => state.ingredientsModule)
-  const [mealToEdit, setMealToEdit] = useState(mealService.getEmptyMeal())
+  const { ingredients } = useSelector((state) => state.ingredientModule)
+
   const navigate = useNavigate()
   const { mealId } = useParams()
 
+  const [mealToEdit, setMealToEdit] = useState(mealService.getEmptyMeal())
+  const [carbSource, setCarbSource] = useState('')
+
   useEffect(() => {
-    if (mealId) {
-      const currentMeal = mealService.getMealById(mealId)
-      setMealToEdit({ ...currentMeal })
-    }
+    if (mealId) loadMeal()
   }, [])
 
-  const onSubmitForm = (data) => {
-    console.log(data)
-    reset()
+  const loadMeal = async () => {
+    const currentMeal = await mealService.getMealById(mealId)
+    setMealToEdit({ ...currentMeal })
   }
+
+  const getIngName = (name) => {
+    const ingNameSplited = name.split('-')
+    let ingName = ''
+    ingNameSplited.map((partName, idx) => {
+      if (idx > 0) ingName += ' '
+      ingName += partName.charAt(0).toUpperCase() + partName.slice(1)
+    })
+    return ingName
+  }
+
+  const handleChange = ({ target }) => {
+    console.log(target.value)
+  }
+  const onPickCarbSource = ({ target }) => {
+    const { name, value } = target
+    setCarbSource(value)
+  }
+
+  const onSubmitForm = (values) => {
+    saveMeal(values)
+    navigate(-1)
+  }
+
+  const editSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(20, 'Too Long!')
+      .required('Required'),
+    // carb: Yup.string().required('Must pick a carb source'),
+  })
+
   return (
     <section className='meal-edit'>
       <button onClick={() => navigate(-1)}>Back</button>
-      <form onSubmit={handleSubmit(onSubmitForm)}>
-        <div className='meal-name-box'>
-          <label htmlFor='name'>Meal name:</label>
-          <input
-            type='text'
-            placeholder='Enter meal name'
-            {...register('name', { required: 'Please enter meal name' })}
-          />
-          {errors.name && (
-            <span className='form-error'>{errors.name.message}</span>
-          )}
-        </div>
-      </form>
+      <Formik
+        initialValues={{ ...mealToEdit }}
+        enableReinitialize
+        validationSchema={editSchema}
+        onSubmit={onSubmitForm}
+      >
+        {({ errors, touched }) => (
+          <Form className='formik flex column justify-center align-center'>
+            <label htmlFor='name'>
+              Meal name:
+              <Field
+                name='name'
+                id='name'
+                value={mealToEdit.name}
+                placeholder="Meal's name"
+              />
+            </label>
+            {errors.name && touched.name ? <span>{errors.name}</span> : null}
+
+            <button className='btn' type='submit'>
+              Save
+            </button>
+          </Form>
+        )}
+      </Formik>
     </section>
   )
 }
