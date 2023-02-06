5fef3f36a9b6444b746fd976526335c031a1752a
Kai Nagar <kaynagar@gmail.com>
2023-02-06 14:04:05 +0200

    started react-hook-form MealEdit

--- Frontend/src/pages/MealEdit.jsx
+++ Frontend/src/pages/MealEdit.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useForm } from 'react-hook-form'
 import { mealService } from '../services/meal.service'
 
 export const MealEdit = () => {
-  const [mealToEdit, setMealToEdit] = useState(mealService.getEmptyMeal())
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm()
   const { ingredients } = useSelector((state) => state.ingredientsModule)
-  console.log(ingredients)
-
+  const [mealToEdit, setMealToEdit] = useState(mealService.getEmptyMeal())
   const navigate = useNavigate()
   const { mealId } = useParams()
 
@@ -17,12 +23,26 @@ export const MealEdit = () => {
       setMealToEdit({ ...currentMeal })
     }
   }, [])
+
+  const onSubmitForm = (data) => {
+    console.log(data)
+    reset()
+  }
   return (
     <section className='meal-edit'>
       <button onClick={() => navigate(-1)}>Back</button>
-      <form>
-        <label htmlFor='meal-name'>Meal name:</label>
-        <input type='text' id='meal-name' placeholder='Enter meal name' />
+      <form onSubmit={handleSubmit(onSubmitForm)}>
+        <div className='meal-name-box'>
+          <label htmlFor='name'>Meal name:</label>
+          <input
+            type='text'
+            placeholder='Enter meal name'
+            {...register('name', { required: 'Please enter meal name' })}
+          />
+          {errors.name && (
+            <span className='form-error'>{errors.name.message}</span>
+          )}
+        </div>
       </form>
     </section>
   )
